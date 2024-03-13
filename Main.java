48888f3852d640115e7daac8732a20896224f681
suryakant90 <62537489+suryakant90@users.noreply.github.com>
2024-03-13 17:00:16 +0530

    Variable

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String args[])
+    {
+        // Variable
+        String name = "Gaurnaga";
+        int age= 32;
+        double price = 22.5;
+        int a = 25;
+        int b = 10;
+        
+        
+        b =20;
+        name = "Gauranga";
+        
+
+
+    }
+}
