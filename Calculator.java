d2bcb7e5dd56358f7fa79b5a9cec84b841f263cb
suryakant90 <62537489+suryakant90@users.noreply.github.com>
2024-03-14 00:55:59 +0530

    Variable

---
+++ Calculator.java
@@ -0,0 +1,45 @@
+// Make a Calculator. Take 2 numbers (a & b) from the user and an operation as follows :
+//	1 : + (Addition) a + b
+//2 : - (Subtraction) a - b
+//3 : * (Multiplication) a * b
+//4 : / (Division) a / b
+//5 : % (Modulo or remainder) a % b
+//Calculate the result according to the operation given and display it to the user.
+
+import java.util.Scanner;
+import java.util.Scanner.*;
+//import java.util.Scanner.*;
+
+import static java.lang.System.in;
+
+
+public class Calculator {
+    public static void main(String[] args){
+
+            //float = Object number;
+
+
+        //System.out.println("Enter your desired number" );
+        Scanner number = new Scanner(System.in);
+      //10  float button = number.nextInt();
+        System.out.println("Press 1 for addition,Press 2 for substraction,Press 3 for multiplication,Press 4 for division,Press 5 for Reminder ");
+
+        // new= c;
+        System.out.println("Enter First Value");
+        float a = number.nextFloat();
+        System.out.println("Enter Second Value");
+        float b = number.nextFloat();
+//        System.out.println("Press 1 for addition ");
+//        System.out.println("Press 2 for Subtraction ");
+//        System.out.println("Press 3 for Multiplication ");
+//        System.out.println("Press 4 for Division ");
+//        System.out.println("Press 5 for Reminder ");
+
+
+
+
+        }
+    }
+
+
+
