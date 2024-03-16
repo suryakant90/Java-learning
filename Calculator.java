d982cef126bf1e810a904bd190b33df20f896e64
suryakant90 <62537489+suryakant90@users.noreply.github.com>
2024-03-17 00:19:53 +0530

    Variable

--- Calculator.java
+++ Calculator.java
@@ -1,45 +1,60 @@
-// Make a Calculator. Take 2 numbers (a & b) from the user and an operation as follows :
-//	1 : + (Addition) a + b
-//2 : - (Subtraction) a - b
-//3 : * (Multiplication) a * b
-//4 : / (Division) a / b
-//5 : % (Modulo or remainder) a % b
-//Calculate the result according to the operation given and display it to the user.
-
 import java.util.Scanner;
-import java.util.Scanner.*;
-//import java.util.Scanner.*;
-
-import static java.lang.System.in;
 
 
-public class Calculator {
-    public static void main(String[] args){
+public class Calculator
+{
 
-            //float = Object number;
+    public static void main(String[] args)
+    {
 
+        Scanner scan = new Scanner(System.in);
 
-        //System.out.println("Enter your desired number" );
-        Scanner number = new Scanner(System.in);
-      //10  float button = number.nextInt();
-        System.out.println("Press 1 for addition,Press 2 for substraction,Press 3 for multiplication,Press 4 for division,Press 5 for Reminder ");
-
-        // new= c;
         System.out.println("Enter First Value");
-        float a = number.nextFloat();
+        float a;
+        a = scan.nextFloat();
         System.out.println("Enter Second Value");
-        float b = number.nextFloat();
-//        System.out.println("Press 1 for addition ");
-//        System.out.println("Press 2 for Subtraction ");
-//        System.out.println("Press 3 for Multiplication ");
-//        System.out.println("Press 4 for Division ");
-//        System.out.println("Press 5 for Reminder ");
+        float b = scan.nextFloat();
+
+        System.out.println("Choose operations(+,-,*,/,%): ");
+        char operator = scan.next().charAt(0);
+
+        float result;
 
 
-
+        switch (operator)
+        {
+            case '+':
+                result = a + b;
+                System.out.println(result);
+                break;
+            case '-':
+                result = a - b;
+                System.out.println(result);
+                break;
+            case '*':
+                result = a * b;
+                System.out.println(result);
+                break;
+            case '/':
+                if (b == 0)
+                {
+                    System.out.println("∞");
+                } else
+                {
+                    result = a / b;
+                    System.out.println(result);
+                }
+                break;
+            case '%':
+                result = a % b;
+                System.out.println(result);
+                break;
 
         }
+
+
     }
+}
 
 
 
--- Main.java
+++ Main.java
@@ -1,18 +1,33 @@
+import java.util.*;
+
+//import static java.lang.System.in;
+
 public class Main {
 
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         // Variable
-        String name = "Gaurnaga";
-        int age= 32;
-        double price = 22.5;
-        int a = 25;
-        int b = 10;
-        
-        
-        b =20;
-        name = "Gauranga";
-        
+
+        //int a= 10;
+        //int b = 20;
+        //int sum = a+b;
+        //System.out.println(sum);
+        //int diff = a-b;
+        // System.out.println(diff);
+        // int ans = (a*b) / (a-b) ;
+
+       // System.out.println(in);
+        Scanner sc = new Scanner(System.in);
+      //  String name = sc.nextLine();
+        // type of Input
+        //next (in, floot,string.....)
+        //System.out.println(name);
+        int a = sc.nextInt();
+        System.out.println("a");
+        int b = sc.nextInt();
+        System.out.println("b");
+        int add = a+b;
+        System.out.println(add);
+
 
 
     }
