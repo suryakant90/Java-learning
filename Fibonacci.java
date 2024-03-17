b2dcbbc6022af9f0842d2ff66d183d3cb94ecd02
suryakant90 <62537489+suryakant90@users.noreply.github.com>
2024-03-18 01:10:18 +0530

    Variable

---
+++ Fibonacci.java
@@ -0,0 +1,42 @@
+import com.sun.security.jgss.GSSUtil;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        int c = a+b;
+        System.out.println(0);
+        System.out.println(1);
+        System.out.println(c);
+
+        a=b;
+        b=c;
+        c=a+b;
+
+        System.out.println(c);
+        a=b;
+        b=c;
+        c=a+b;
+
+        System.out.println(c);
+        a=b;
+        b=c;
+        c=a+b;
+
+        System.out.println(c);
+
+
+
+
+//        for (int i=0; i>=100; i++  );
+//        if(i());
+
+    }
+
+
+
+
+
+
+
+}
---
+++ Loops.java
@@ -0,0 +1,31 @@
+public class Loops
+    {
+        public static void main(String[] args)
+        {
+            int a =1;
+            for (int i = 1; i <=10; i++)
+                if(i%2 == 0)
+                    {
+                        System.out.println(i);
+                    }
+//                else {
+//                        System.out.println();
+//                     }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
