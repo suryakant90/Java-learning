34e977d2713db02264c64ceae283bc0f86679a4e
suryakant90 <62537489+suryakant90@users.noreply.github.com>
2024-03-19 01:26:39 +0530

    Variable

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,30 +1,48 @@
-import com.sun.security.jgss.GSSUtil;
-
+import java.time.Year;
+import java.util.Scanner;
 public class Fibonacci {
     public static void main(String[] args) {
-        int a = 0;
-        int b = 1;
-        int c = a+b;
-        System.out.println(0);
-        System.out.println(1);
-        System.out.println(c);
+        Scanner scanner = new  Scanner(System.in);
 
-        a=b;
-        b=c;
-        c=a+b;
+        System.out.println("Enter number for Fib series");
+         int Fib = scanner.nextInt();
 
-        System.out.println(c);
-        a=b;
-        b=c;
-        c=a+b;
+        int a=0 , b=1;
 
-        System.out.println(c);
-        a=b;
-        b=c;
-        c=a+b;
+       System.out.println(a+" "+b+" "+ "initial start");
 
-        System.out.println(c);
 
+
+       for( int i=1; i<=Fib; i++ )
+       {
+       int c = a+b;
+           System.out.println(int c,"*");
+           a=b;
+           b=c;
+       }
+
+//        System.out.println("If you need ratio Press 1 else 2");
+//        int 1=1;
+//        int x;
+//        //char y;
+//
+//       if(){
+//           int x= 1
+//
+//                for( int i=3; i<=Fib; i++ )
+//                    {
+//                        int c = (a+b);
+//
+//                        a=b;
+//                        b=c;
+//                        c=a%b;
+//                        System.out.println(c);
+//
+//                    }
+//            }
+//       else {
+//            System.out.println("Thank You");
+//            }
 
 
 
